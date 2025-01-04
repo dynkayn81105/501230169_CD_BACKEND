221bb1a6a0ac9087eb1017bb871ff5b91841be79
unknown <501230169@stu.itc.edu.vn>
2025-01-04 10:45:54 +0700

    create layout

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 import express from 'express';
-const app=express();
-const port =5001
-
-app.get('/', (req,res) => {
-    res.send('Hello World!');
+import path from 'path';
+const app = express();
+const port = 5002
+const __dirname = path.resolve()
+app.use("/static", express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set("views",__dirname + "/src/views")
+app.get('/', (req, res) => {
+   res.render("pages/indexs")
+});
+app.get("/components", (req, res) => {
+    res.render("pages/components")
+})
+app.get("/forms", (req, res) => {
+    res.render("pages/forms")
+});
+app.get("/icons", (req, res) => {
+    res.render("pages/icons")
 });
 
-app.listen(port, function(){
-    console.log('http://localhost'+port);
-})
+app.get("/tables", (req, res) => {
+    res.render("pages/tables")
+});
+
+app.get("/notifications", (req, res) => {
+    res.render("pages/notifications")
+});
+
+app.get("/typographys", (req, res) => {
+    res.render("pages/typographys")
+});
+app.listen(port, function() {
+    console.log('http://localhost:' + port);
+});
